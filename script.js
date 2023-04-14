1ad71489a328ad02433c46e62ce7e1877445a47f
DarioMedugno <>
2023-04-14 19:22:42 +0200

    progetto web con registrazione completato

---
+++ script.js
@@ -0,0 +1,15 @@
+let logregBox = document.querySelector('.logreg-box');
+let loginLink = document.querySelector('.login-link'); 
+let registerLink = document.querySelector('.register-link');
+
+
+registerLink.addEventListener('click', () => {
+    logregBox.classList.add('active');
+});
+
+
+loginLink.addEventListener('click', () => {
+    logregBox.classList.remove('active');
+});
+
+
